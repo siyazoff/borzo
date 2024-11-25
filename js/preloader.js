c27c68b97168e64a7fbca116aa4aedbd8c87a834
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-11-25 14:31:59 +0500

    cleaning commit

--- js/preloader.js
+++ js/preloader.js
@@ -1,41 +1,65 @@
-// let tl = gsap.timeline();
+if (!sessionStorage.getItem("preloaderShown")) {
+  //   $(".preloader").addClass("show");
+  setTimeout(function () {
+    $(".preloader").addClass("show");
+    sessionStorage.setItem("preloaderShown", "true");
+  }, 5000);
+} else {
+  $(".preloader").hide();
+}
+// if (!sessionStorage.getItem("preloaderShown")) {
+//   //   showPreloader();
+//   $(".preloader").toggleClass("show");
+//   sessionStorage.setItem("preloaderShown", "true");
+// } else {
+//   $(".preloader").hide();
+// }
 
-// tl.to("body", {
-//   overflow: "inherit",
-// })
-//   .to(".preloader .text-container", {
-//     duration: 0,
+// function showPreloader() {
+//   let tl = gsap.timeline();
+
+//   tl.to(".preloader", {
+//     display: "flex",
 //     opacity: 1,
-//     ease: "Power3.easeOut",
 //   })
-//   .from(".preloader .text-container svg .item-preloader-1", {
-//     duration: 1,
-//     delay: 0.5,
-//     y: 70,
-//     skewY: 50,
-//     stagger: 0.2,
-//     ease: "Power3.easeOut",
-//   })
-//   .from(".preloader .text-container svg .item-preloader-2", {
-//     duration: 1,
-//     delay: 0.5,
-//     x: -150,
-//     // skewX: 50,
-//     stagger: 0.1,
-//     ease: "Power3.easeOut",
-//   })
-//   .to(".preloader", {
-//     duration: 1,
-//     height: "0vh",
-//     ease: "Power3.easeOut",
-//   })
-//   .to(
-//     "body",
-//     {
-//       overflow: "unset",
-//     },
-//     "-=2"
-//   )
-//   .to(".preloader", {
-//     display: "none",
-//   });
+//     .to("body", {
+//       overflow: "hidden",
+//     })
+//     .to(".preloader .text-container", {
+//       duration: 0,
+//       opacity: 1,
+//       ease: "Power3.easeOut",
+//     })
+//     .from(".preloader .text-container svg .item-preloader-1", {
+//       duration: 1,
+//       delay: 0.5,
+//       y: 70,
+//       skewY: 50,
+//       stagger: 0.2,
+//       ease: "Power3.easeOut",
+//     })
+//     .from(".preloader .text-container svg .item-preloader-2", {
+//       duration: 1,
+//       delay: 0.5,
+//       x: -150,
+//       stagger: 0.1,
+//       ease: "Power3.easeOut",
+//     })
+//     .to(".preloader", {
+//       duration: 1,
+//       height: "0vh",
+//       ease: "Power3.easeOut",
+//       onComplete: function () {
+//         if (sessionStorage.getItem("preloaderShown")) {
+//           $(".preloader").hide(); // Скрываем только если еще не было скрыто
+//         }
+//       },
+//     })
+//     .to(
+//       "body",
+//       {
+//         overflow: "unset",
+//       },
+//       "-=2"
+//     );
+// }
